b7d7893dbe9fb0830c10f349563398d20cd9411e
Adar <adar9800@gmail.com>
2024-01-26 11:18:31 +0200

    Changes in file Hello.java number 5

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ public class Hello {
         for (int i = 0; i <= 10; i++) {
             System.out.println("Hello Java World... " + i);
         }
-        System.out.println("Job numer One Done");
+        System.out.println("Job number One Done");
+        System.out.println("now Job number Two");
+
     }
 }
