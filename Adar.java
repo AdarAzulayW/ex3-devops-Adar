80ac486eccb15ddd2a6542c2c558c139e53a5dfe
Adar <adar9800@gmail.com>
2024-01-26 10:49:09 +0200

    All files to Add to the repo

---
+++ Adar.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main (String[] args) {
+        for (int i=0; i<=5; i++)
+        {
+            (if i==0){
+                System.out.println("My name is Adar Azulay " + i);
+            }
+            (if i >0){
+                System.out.println("this is exs number 3 for devops " + i);
+            }
+            
+        }
+    }
+}
